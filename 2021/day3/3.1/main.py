1655b16119549998c985495b387aa2ecc0fbcc27
Oscar Börjesson <oscar.boerjesson@gmail.com>
2021-12-03 06:51:07 +0100

    added day 3

---
+++ 2021/day3/3.1/main.py
@@ -0,0 +1,32 @@
+def readFile(filepath):
+    with open(filepath) as file:
+        lines = file.readlines()
+    return lines
+
+
+lines = readFile('input')
+gamma = ''
+epsilon = ''
+mostCommonBit = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+
+for line in lines:
+    count = 0
+    for char in line:
+        if char == '1':
+            mostCommonBit[count] += 1
+        count += 1
+
+
+for bit in mostCommonBit:
+    if bit > 500:
+        gamma += '1'
+        epsilon += '0'
+    else:
+        gamma += '0'
+        epsilon += '1'
+
+
+gamma = int(gamma, 2)
+epsilon = int(epsilon, 2)
+print(gamma*epsilon)
---
+++ 2021/day3/3.2/main.py
@@ -0,0 +1,99 @@
+def readFile(filepath):
+    with open(filepath) as file:
+        lines = file.readlines()
+    return lines
+
+
+def gameps(lines):
+    gamma = ''
+    epsilon = ''
+    mostCommonBit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    for line in lines:
+        count = 0
+        for char in line:
+            if char == '1':
+                mostCommonBit[count] += 1
+            count += 1
+
+    for bit in mostCommonBit:
+        # Possible issues with equally common bits
+        if bit >= len(lines)/2:
+            gamma += '1'
+            epsilon += '0'
+        else:
+            gamma += '0'
+            epsilon += '1'
+    return [gamma, epsilon]
+
+
+lines = readFile('input')
+gamma = ''
+epsilon = ''
+mostCommonBit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+for line in lines:
+    count = 0
+    for char in line:
+        if char == '1':
+            mostCommonBit[count] += 1
+        count += 1
+
+
+for bit in mostCommonBit:
+    # Possible issues with equally common bits
+    if bit >= len(lines)/2:
+        gamma += '1'
+        epsilon += '0'
+    else:
+        gamma += '0'
+        epsilon += '1'
+
+
+# Oxygen
+oldLines = lines
+newLines = []
+counter = 0
+while counter < 12:
+    for line in oldLines:
+        # Check every char in pos counter for commonality
+        # If char checks out append line to newLines
+        if line[counter] == gamma[counter]:
+            newLines.append(line)
+    counter += 1
+
+    # Check for final answer
+    if len(newLines) == 1:
+        break
+    # On non-final answer, reset
+    oldLines = newLines
+    newLines = []
+    gamma = gameps(oldLines)[0]
+oxygen = newLines[0]
+
+# CO2 Scrubber
+oldLines = lines
+newLines = []
+counter = 0
+while counter < 12:
+    for line in oldLines:
+        # Check every char in pos counter for commonality
+        # If char checks out append line to newLines
+        if line[counter] == epsilon[counter]:
+            newLines.append(line)
+    counter += 1
+
+    # Check for final answer
+    if len(newLines) == 1:
+        break
+    # On non-final answer, reset
+    oldLines = newLines
+    newLines = []
+    epsilon = gameps(oldLines)[1]
+co2 = newLines[0]
+
+
+print(int(oxygen, 2)*int(co2, 2))
+
+
