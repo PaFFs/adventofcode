28b15adb57e4c9ef01d5a4908560943fd891bf25
Oscar Börjesson <oscar.boerjesson@gmail.com>
2022-12-01 12:32:34 +0100

    day1 solved

--- 2022/day1/main.py
+++ 2022/day1/main.py
@@ -1,3 +1,15 @@
 import common
 
 f = common.readFile('indata')
+
+elf = 0
+elves = []
+for line in f:
+    if line != '\n':
+        elf += int(line)
+    else:
+        elves.append(elf)
+        elf = 0
+
+elves.sort()
+print(elves[-1]+elves[-2]+elves[-3])
